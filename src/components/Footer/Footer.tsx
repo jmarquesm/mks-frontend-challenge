d64f9a28984df95ca1522e01f8fa67bad57a685e
Joao Marques <jmarquesm@users.noreply.github.com>
2023-01-30 21:59:12 -0400

    feat: add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+// styles
+import * as S from './styles'
+
+export function Footer() {
+  return (
+    <S.Container>
+      <S.CompanyRights>MKS sistemas © Todos os direitos reservados</S.CompanyRights>
+    </S.Container>
+  )
+}
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,19 @@
+// vendors
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-color: #eeeeee;
+  width: 100%;
+  bottom: 0;
+  height: 34px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: auto;
+`
+
+export const CompanyRights = styled.div`
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 15px;
+`
